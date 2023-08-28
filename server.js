c6d860b17011506f4a0981b93acf9fadb8059cb5
elbahjacharafeddine <101811374+elbahjacharafeddine@users.noreply.github.com>
2023-08-28 14:45:30 +0100

    notifications with RabbitMQ

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const passport = require("passport");
 const path = require("path");
 const FollowedUser = require("./models/followed-user");
 const app = express();
+const https = require('https')
 app.use(express.json());
 const corsOptions = {
     origin: ['https://rs-client-master.vercel.app', 'http://localhost:3000']
@@ -101,12 +102,14 @@ const Laboratory = require("./models/laboratory");
 const Team = require("./models/team");
 const TeamMemberShip = require("./models/team-membership");
 const User = require("./models/user");
-app.get('/listen-to-rabbit',(req, res) =>{
+const {response} = require("express");
+// app.get('/listen-to-rabbit',(req, res) =>{
+
     amqp.connect('amqps://sosytgab:jPleCfcPHfayJEgRoLXeDgVgyt3aBd_0@rattlesnake.rmq.cloudamqp.com/sosytgab',(error0,connection) =>{
         if (error0){
             throw error0
         }
-        console.log("connected to RabbitMQ ...")
+        console.log("connected to RabbitMQ from rs-backend ...")
         connection.createChannel((error1, channel) =>{
             if (error1) {
                 throw error1
@@ -118,14 +121,11 @@ app.get('/listen-to-rabbit',(req, res) =>{
                 // const listOfObjects = JSON.parse(jsonList);
                 console.log(message.content.toString('utf8'));
                 // res.send(message.content.toString('utf-8'))
+                channel.ack(message);
             })
         })
     })
-    process.on('beforeExit',() =>{
-        console.log("closed the connection RabbitMQ")
-
-    })
-})
+// })
 
 
 
@@ -182,168 +182,42 @@ app.get('/get-followed-users',async (req, resp) => {
             user_id : e.user_id,
             roles :e.roles,
         }));
-        resp.status(200).send(r);
-    }
-
-    if (teamAbbreviation) {
-        const team = await Team.findOne({
-            abbreviation: teamAbbreviation,
-        });
-
-        const teamsMemberShips = await TeamMemberShip.find({
-            team_id: team._id,
-            active: true,
-            user_id: { $in: followedUsersIds },
-        });
-
-        const followedUsers = await Promise.all(teamsMemberShips.map(({ user_id }) => FollowedUser.findOne({ user_id })));
-
-        const followedUsersAcounts = await Promise.all(teamsMemberShips.map(({ user_id }) => User.findById(user_id)));
-
-        const result = followedUsersAcounts.map(({ firstName, lastName }, index) => ({
-            ...followedUsers[index]._doc,
-            firstName,
-            lastName,
-        }));
-        resp.status(200).send(result);
-    }
-})
-
-
-
-app.get('/prof/scopus/:authorId',async (req, res) =>{
-    const {authorId} = req.params
-    const browser = await getBrowser();
-    const page = await browser.newPage();
-    try {
-        // Définir l'en-tête User-Agent personnalisé
-        await page.setUserAgent('Chrome/96.0.4664.93');
-        await page.setDefaultNavigationTimeout(85000);
-        // await page.waitForFunction(() => document.readyState === 'complete');
-        // const navigationPromise = page.waitForNavigation({ waitUntil: 'domcontentloaded' });
-
-        await page.goto('https://www.scopus.com/authid/detail.uri?authorId=' + authorId);
-        // await navigationPromise; // Wait for the DOM content to be fully loaded
-
-        console.log('navigation to scopus...')
-
-        await page.waitForSelector('#scopus-author-profile-page-control-microui__general-information-content',{timeout:4000});
-
-        // await page.waitForSelector('.container .AuthorProfilePageControl-module__sgqt5',{ timeout: 3000 })
-        // page.waitForTimeout(1000)
-        // const name = await page.$eval('#scopus-author-profile-page-control-microui_general-information-content > div.Col-module_hwM1N.offset-lg-2 > div > h1 > strong', (e) => e.textContent.trim().replace(',',''))
-        const name =''
-        // await page.waitForSelector('#scopus-author-profile-page-control-microui__general-information-content')
-        // const univer = await page.$eval('#scopus-author-profile-page-control-microui_general-information-content > div.Col-modulehwM1N.offset-lg-2 > ul > li.AuthorHeader-moduleDRxsE > span > a > span.Typography-modulelVnit.Typography-moduleNfgvc.Button-module_Imdmt', (e) => e.textContent.trim())
-        const univer =''
-        let h_index=''
-
-        try {
-            h_index = await page.$eval("#scopus-author-profile-page-control-microui_general-information-content > div.Col-modulehwM1N.offset-lg-2 > section > div > div:nth-child(3) > div > div > div:nth-child(1) > span.Typography-modulelVnit.Typography-moduleix7bs.Typography-module_Nfgvc",(e) =>e.textContent)
-        }catch (error){
-            console.log("")
+        const options ={
+            hostname : 'rs-scraper-master.onrender.com',
+            path : '/prof/scopus',
+            method : 'GET'
         }
-        const interests = []
-
-        await page.waitForTimeout(1000);
-        console.log('start scrolling...')
-        await autoScroll(page);
-        console.log('End of scrolling...')
-        await page.waitForTimeout(1500);
-        const publications = await page.evaluate(() =>
-            Array.from(document.querySelectorAll('.ViewType-module__tdc9K li'), (e) => ({
-                title:e.querySelector('h4 span').innerText,
-                authors: Array.from((new Set(Array.from(e.querySelectorAll('.author-list span'), (authorElement) => authorElement.innerText)))),
-                citation : e.querySelector('.col-3 span:nth-child(1)').innerText,
-                year:e.querySelector('.text-meta span:nth-child(2)').innerText.replace('this link is disabled',"").substring(0,4),
-                source:e.querySelector('span.text-bold').innerText,
-            })));
-
-        const allPath = await page.evaluate(() => Array.from(document.querySelectorAll('path[aria-label]'), (e) => e.getAttribute('aria-label')));
+        await  processRequests(r,options)
 
 
-        const citationsPerYear = allPath.map(item => {
-            const [yearString, citationsString] = item.split(':');
-            const year = parseInt(yearString.trim());
-            const citations = parseInt(citationsString.trim());
-
-            return { year, citations };
-        });
-        const totalCitations = citationsPerYear.reduce((acc, item) => acc + item.citations, 0);
-        const indexes = [
-            {
-                name: "citations",
-                total: totalCitations,
-                lastFiveYears: "",
-            },
-            {
-                name: "h-index",
-                total: h_index,
-                lastFiveYears: "",
-            },
-        ];
-
-        // await page.waitForTimeout(1000);
-
-
-        const author ={
-            name,
-            profilePicture: "",
-            univer,
-            email: "",
-            indexes,
-            interests,
-            publications,
-            coauthors: [],
-            citationsPerYear,
-        };
-//
-        // res.header('Access-Control-Allow-Origin', 'https://rs-client-master.vercel.app');
-        res.send({ "author": { authorId, platform: "scopus", ...author } });
-        console.log("the response has been sent")
-
-
-    } catch (error) {
-        console.error('Une erreur s\'est produite :', error);
     }
-    finally {
-        // let pages = await browser.pages();
-        // await Promise.all(pages.map(page =>page.close()));
-        await page.close()
-        await browser.close()
-    }
+
 })
 
 
-let browser;
-const puppeteer = require('puppeteer')
-// Function to launch the Puppeteer browser if not already launched.
-async function getBrowser() {
-    browser = await puppeteer.launch({
-        headless: true,
-        userDataDir: false,
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+async function sendRequest(options){
+    let result =''
+        const request = https.request(options,response =>{
+            response.on('data', chunk => {
+                result += chunk;
+            });
+
+            response.on('end', () => {
+                result = JSON.parse(result)
+            });
+        })
+    request.on('error', error => {
+        console.error("error for scraping data");
     });
 
-    return browser;
+    request.end();
+    console.log("result of this author "+ options.path + " :"+result)
+    return result
 }
 
-
-async function autoScroll(page){
-    await page.evaluate(async () => {
-        await new Promise((resolve) => {
-            let totalHeight = 0;
-            const distance = 100;
-            const timer = setInterval(() => {
-                const scrollHeight = document.body.scrollHeight;
-                window.scrollBy(0, distance);
-                totalHeight += distance;
-
-                if (totalHeight >= scrollHeight - window.innerHeight) {
-                    clearInterval(timer);
-                    resolve();
-                }
-            }, 100);
-        });
-    });
+async function processRequests(data,options) {
+        for (const e of data){
+            options.path = options.path +"/"+data.authorId
+            await sendRequest(options)
+        }
 }
